1768829970a0cd7b887d1c9022245ad1e0dcf69d
droksty <droksty@gmail.com>
2026-08-01 18:47:57 +0300

    initial commit

---
+++ src/main/java/droksty/nurseshifts/NurseShiftsApplication.java
@@ -0,0 +1,13 @@
+package droksty.nurseshifts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurseShiftsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NurseShiftsApplication.class, args);
+	}
+
+}
